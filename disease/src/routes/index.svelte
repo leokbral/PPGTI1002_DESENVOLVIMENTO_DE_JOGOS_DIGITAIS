494bcb9224ded50a4a84dde3d4d86943605b52c2
leokbral <83135257+leokbral@users.noreply.github.com>
2022-01-22 12:31:55 -0300

    Update index.svelte

--- disease/src/routes/index.svelte
+++ disease/src/routes/index.svelte
@@ -3,26 +3,60 @@
 </script>
 
 <div class="main" style="--bgImg: url({menucoverImg});">
-    <div class="subtitle" />
-    <p>Como <a href="./comojogar">jogar</a></p>
-    <p>Jogue com o <a href="./invasor">INVASOR</a></p>
-    <p>Jogue com o <a href="./defensor">DEFENSOR</a></p>
+    <div class="menu">
+        <button
+            class="button"
+            on:click={() => (window.location.href = "./invasor")}
+            >Jogar com o Invasor</button
+        >
+        <br />
+        <br />
+        <button
+            class="button"
+            on:click={() => (window.location.href = "./defensor")}
+            >Jogar com o Defensor</button
+        >
+        <br />
+        <br />
+        <button
+            class="button"
+            on:click={() => (window.location.href = "./comojogar")}
+            >Como jogar</button
+        >
+    </div>
 </div>
 
 <style>
     .main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
         height: 97vh;
         background-image: var(--bgImg);
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
     }
-    .subtitle {
-        font-size: 10px;
-        line-height: 1.3%;
+    .menu {
+        grid-column: 2;
+        grid-row: 2;
+        padding-left: 70px;
+        padding-top: 20px;
+    }
+
+    .button {
+        border: none;
+        color: #ac77ba;
+        /* padding: 15px 110px; */
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        background-color: #4a1079;
+        width: 200px;
+        height: 40px;
+        border-radius: 10px;
     }
 </style>
