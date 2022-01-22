3b2fdb64d7ec3b34c99b1e163a7cffddbe444765
leokbral <83135257+leokbral@users.noreply.github.com>
2022-01-22 10:57:57 -0300

    Update index.svelte

--- disease/src/routes/index.svelte
+++ disease/src/routes/index.svelte
@@ -2,11 +2,8 @@
     import menucoverImg from "$lib/assets/menucover.png";
 </script>
 
-<div class="main" style="--bgImg: url({menucoverImg})">>
-    <h1>DISEASE</h1>
-    <div class="subtitle">
-        <h2>WEB GAME</h2>
-    </div>
+<div class="main" style="--bgImg: url({menucoverImg});">
+    <div class="subtitle" />
     <p>Como <a href="./comojogar">jogar</a></p>
     <p>Jogue com o <a href="./invasor">INVASOR</a></p>
     <p>Jogue com o <a href="./defensor">DEFENSOR</a></p>
@@ -19,7 +16,10 @@
         align-items: center;
         justify-content: center;
         height: 97vh;
-        background-image: url(menucoverImg);
+        background-image: var(--bgImg);
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
     }
     .subtitle {
         font-size: 10px;
